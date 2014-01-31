227007b6aa70b11d155fb8a783edee82a97e56a8
Marijn van Zelst <marijn.vanzelst@trifork.nl>
2014-01-31 17:59:20 +0100

    Added start, stop, limit and reverse parameters

--- frontend-web/src/main/java/org/classified_event_aggregation/web/controller/Action.java
+++ frontend-web/src/main/java/org/classified_event_aggregation/web/controller/Action.java
@@ -2,22 +2,26 @@ package org.classified_event_aggregation.web.controller;
 
 
 import java.util.HashSet;
-import java.util.Iterator;
-import java.util.List;
 import java.util.Set;
 
+import org.springframework.core.convert.support.ConversionServiceFactory;
+import org.springframework.core.convert.support.DefaultConversionService;
+import org.springframework.core.convert.support.GenericConversionService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.datastax.driver.core.BoundStatement;
 import com.datastax.driver.core.Cluster;
 import com.datastax.driver.core.PreparedStatement;
 import com.datastax.driver.core.ResultSet;
 import com.datastax.driver.core.Row;
 import com.datastax.driver.core.Session;
-import com.google.gson.Gson;
+import com.datastax.driver.core.querybuilder.Clause;
+import com.datastax.driver.core.querybuilder.QueryBuilder;
+import com.datastax.driver.core.querybuilder.Select;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonPrimitive;
 
@@ -34,27 +38,44 @@ public class Action {
 		return "rickshaw";
 	}
 
+	@RequestMapping("/classification/{classificationKey}/unique_values")
+	public @ResponseBody String getValuesPerClassificationKey(
+			@PathVariable String classificationKey,
+			@RequestParam(value = "start", defaultValue = "") String startParam,
+			@RequestParam(value = "end", defaultValue = "") String endParam,
+			@RequestParam(value = "reverse", defaultValue = "false") String reverseParam,
+			@RequestParam(value = "limit", defaultValue = "1000") String limitParam
+		){
+		GenericConversionService service = new DefaultConversionService();
+		Long start = service.convert(startParam, Long.class);
+		Long end = service.convert(endParam, Long.class);
+		Boolean reverse = service.convert(reverseParam, Boolean.class);
+		Long limit = service.convert(limitParam, Long.class);
 
-	@RequestMapping("/classification/{classificationKey}")
-	public @ResponseBody String getValuesPerClassificationKey(@PathVariable String classificationKey){
 		Session session = getCassandraConnection();
-		PreparedStatement stmt = session.prepare(
-			"SELECT classification_value " +
-			"FROM event_counters_by_classification_key " +
-			"WHERE " + 
-				"period_type_name = 'year' AND " + 
-				"classification_key = ?;"
-		);
-		ResultSet result = session.execute(
-			stmt.bind(classificationKey)
-		);
-		
+		Select query = QueryBuilder
+			.select("classification_value")
+			.from("event_counters_by_classification_key")
+			.where(QueryBuilder.eq("period_type_name", "year"))
+				.and(QueryBuilder.eq("classification_key", classificationKey))
+			.orderBy((!reverse ? QueryBuilder.asc("period_start") : QueryBuilder.desc("period_start")));
+
+		if(start != null)
+			query.where(QueryBuilder.gt("period_start", start));
+
+		if(end != null)
+			query.where(QueryBuilder.lte("period_start", end));
+
+		ResultSet result = session.execute(query);
 		Set<String> set = new HashSet<String>();
 		for (Row row : result) {
 			set.add(row.getString(0));
 		}
 		JsonArray arr = new JsonArray();
 		for (String string : set) {
+			if(--limit < 0){
+				break;
+			}
 			arr.add(new JsonPrimitive(string));
 		}
 		return arr.toString();
